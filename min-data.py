dadfcd9fbf5b0e1547177f413fc1e63a592316cb
pou <paul.ou@nfl.com>
2016-11-11 12:05:06 -0800

    persist min-data to mongodb

--- min-data.py
+++ min-data.py
@@ -1,4 +1,36 @@
 import requests
+from pymongo import MongoClient
+# from datetime import datetime
+import datetime
+
+ts = 1478874600
+utc_dt = datetime.datetime.utcfromtimestamp(ts)
+print(utc_dt)
+
+client = MongoClient('localhost', 27017)
+db = client.data
+min_datas = db.min_data
 url = "https://www.google.com/finance/getprices?q=AAPL&i=60&p=1d&f=d,o,h,l,c,v"
-yelp_r = requests.get(url)
-print(yelp_r.text)
+symbol = "AAPL"
+response = requests.get(url)
+tokenString = response.text.split("\n")
+
+index = 0
+for token in tokenString[8:]:
+    # prevent from processing empty string
+    if not token:
+        continue
+
+    if index == 0:
+        time = utc_dt
+    else:
+        time_index = token.split(",")[0]
+        time = utc_dt + datetime.timedelta(0, int(time_index) * 60)
+
+    price = token.split(",")[1]
+    min_data = {"_id": symbol + "-" + time.strftime("%Y-%m-%d-%H-%M"),
+                "symbol": symbol,
+                "price": price,
+                "time": time}
+    data_id = min_datas.insert_one(min_data).inserted_id
+    index = index + 1
